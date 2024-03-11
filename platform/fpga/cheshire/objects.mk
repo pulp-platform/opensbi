73f4d2f9473d2435252a6ce71444dc81aa8057b2
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2024-03-11 14:45:47 +0100

    cheshire: fixes

--- platform/fpga/cheshire/objects.mk
+++ platform/fpga/cheshire/objects.mk
@@ -5,4 +5,31 @@
 #		Panagiotis Peristerakis <perister@ics.forth.gr>
 #
 
+# Compiler flags
+platform-cppflags-y =
+platform-cflags-y   =
+platform-asflags-y  =
+platform-ldflags-y  =
+
+# Object to build
 platform-objs-y += platform.o
+
+# Platform XLEN
+PLATFORM_RISCV_XLEN = 64
+
+# Blobs to build
+FW_TEXT_START=0x80000000
+
+# Dynamic firmware configuration
+FW_DYNAMIC=n
+
+# Firmware that jumps at fixed address
+FW_JUMP=n
+# FW_JUMP_ADDR=0x80200000           # This needs to be 2MB aligned for 64-bit support
+# FW_JUMP_FDT_ADDR=0x80800000       # TODO: this is a cancerous workaround; set this correctly, ideally built! 
+
+# Firmware with payload configuration.
+FW_PAYLOAD=y
+# FW_PAYLOAD_ALIGN=0x1000
+FW_PAYLOAD_OFFSET=0x200000        # This needs to be 2MB aligned for 64-bit support
+# FW_PAYLOAD_FDT_ADDR=0x80800000    # TODO: this is a cancerous workaround; set this correctly, ideally built!
--- platform/fpga/cheshire/platform.c
+++ platform/fpga/cheshire/platform.c
@@ -7,6 +7,7 @@
 #include <sbi/riscv_asm.h>
 #include <sbi/riscv_encoding.h>
 #include <sbi/riscv_io.h>
+#include <sbi/sbi_types.h>
 #include <sbi/sbi_console.h>
 #include <sbi/sbi_const.h>
 #include <sbi/sbi_hart.h>
@@ -18,15 +19,15 @@
 #include <sbi_utils/serial/uart8250.h>
 #include <sbi_utils/timer/aclint_mtimer.h>
 
-#define CHESHIRE_UART_ADDR	      0x03002000
-#define CHESHIRE_UART_FREQ	      50000000
+#define CHESHIRE_UART_ADDR	          0x03002000
+#define CHESHIRE_UART_FREQ	          50000000
 #define CHESHIRE_UART_BAUDRATE	      115200
 #define CHESHIRE_UART_REG_SHIFT	      2
 #define CHESHIRE_UART_REG_WIDTH	      4
-#define CHESHIRE_PLIC_ADDR	      0x04000000
+#define CHESHIRE_PLIC_ADDR	          0x04000000
 #define CHESHIRE_PLIC_NUM_SOURCES     20
-#define CHESHIRE_HART_COUNT	      1
-#define CHESHIRE_CLINT_ADDR	      0x02040000
+#define CHESHIRE_HART_COUNT	          1
+#define CHESHIRE_CLINT_ADDR	          0x02040000
 #define CHESHIRE_ACLINT_MTIMER_FREQ   1000000
 #define CHESHIRE_ACLINT_MSWI_ADDR     (CHESHIRE_CLINT_ADDR + 0x0)
 #define CHESHIRE_ACLINT_MTIMER_ADDR   (CHESHIRE_CLINT_ADDR + 0xbff8)
@@ -39,26 +40,26 @@ static struct platform_uart_data uart = {
 };
 
 static struct plic_data plic = {
-	.addr = CHESHIRE_PLIC_ADDR,
+	.addr    = CHESHIRE_PLIC_ADDR,
 	.num_src = CHESHIRE_PLIC_NUM_SOURCES,
 };
 
 static struct aclint_mswi_data mswi = {
-	.addr = CHESHIRE_ACLINT_MSWI_ADDR,
-	.size = ACLINT_MSWI_SIZE,
+	.addr         = CHESHIRE_ACLINT_MSWI_ADDR,
+	.size         = ACLINT_MSWI_SIZE,
 	.first_hartid = 0,
-	.hart_count = CHESHIRE_HART_COUNT,
+	.hart_count   = CHESHIRE_HART_COUNT,
 };
 
 static struct aclint_mtimer_data mtimer = {
-	.mtime_freq = CHESHIRE_ACLINT_MTIMER_FREQ,
-	.mtime_addr = CHESHIRE_ACLINT_MTIMER_ADDR,
-	.mtime_size = 8,
-	.mtimecmp_addr = CHESHIRE_ACLINT_MTIMECMP_ADDR,
-	.mtimecmp_size = 16,
-	.first_hartid = 0,
-	.hart_count = CHESHIRE_HART_COUNT,
-	.has_64bit_mmio = FALSE,
+	.mtime_freq     = CHESHIRE_ACLINT_MTIMER_FREQ,
+	.mtime_addr     = CHESHIRE_ACLINT_MTIMER_ADDR,
+	.mtime_size     = 8,
+	.mtimecmp_addr  = CHESHIRE_ACLINT_MTIMECMP_ADDR,
+	.mtimecmp_size  = 16,
+	.first_hartid   = 0,
+	.hart_count     = CHESHIRE_HART_COUNT,
+	.has_64bit_mmio = 0,
 };
 
 /*
@@ -66,18 +67,6 @@ static struct aclint_mtimer_data mtimer = {
  */
 static int cheshire_early_init(bool cold_boot)
 {
-	void *fdt;
-	struct platform_uart_data uart_data;
-	int rc;
-
-	if (!cold_boot)
-		return 0;
-	fdt = fdt_get_address();
-
-	rc = fdt_parse_uart8250(fdt, &uart_data, "ns16550a");
-	if (!rc)
-		uart = uart_data;
-
 	return 0;
 }
 
@@ -86,14 +75,6 @@ static int cheshire_early_init(bool cold_boot)
  */
 static int cheshire_final_init(bool cold_boot)
 {
-	void *fdt;
-
-	if (!cold_boot)
-		return 0;
-
-	fdt = fdt_get_address();
-	fdt_fixups(fdt);
-
 	return 0;
 }
 
@@ -102,34 +83,16 @@ static int cheshire_final_init(bool cold_boot)
  */
 static int cheshire_console_init(void)
 {
-	return uart8250_init(uart.addr,
-			     uart.freq,
-			     uart.baud,
-			     CHESHIRE_UART_REG_SHIFT,
-			     CHESHIRE_UART_REG_WIDTH);
+	return uart8250_init(uart.addr, 
+						 uart.freq,
+						 uart.baud,
+						 CHESHIRE_UART_REG_SHIFT,
+						 CHESHIRE_UART_REG_WIDTH,
+						 0);
 }
 
 static int plic_cheshire_warm_irqchip_init(int m_cntx_id, int s_cntx_id)
 {
-//  size_t i, ie_words = CHESHIRE_PLIC_NUM_SOURCES / 32 + 1;
-
-	/* By default, enable all IRQs for M-mode of target HART */
-//  if (m_cntx_id > -1) {
-//  	for (i = 0; i < ie_words; i++)
-//  		plic_set_ie(&plic, m_cntx_id, i, 1);
-//  }
-//  /* Enable all IRQs for S-mode of target HART */
-//  if (s_cntx_id > -1) {
-//  	for (i = 0; i < ie_words; i++)
-//  		plic_set_ie(&plic, s_cntx_id, i, 1);
-//  }
-//  /* By default, enable M-mode threshold */
-//  if (m_cntx_id > -1)
-//  	plic_set_thresh(&plic, m_cntx_id, 1);
-//  /* By default, disable S-mode threshold */
-//  if (s_cntx_id > -1)
-//  	plic_set_thresh(&plic, s_cntx_id, 0);
-
 	return plic_warm_irqchip_init(&plic, m_cntx_id, s_cntx_id);
 }
 
@@ -185,20 +148,21 @@ static int cheshire_timer_init(bool cold_boot)
  * Platform descriptor.
  */
 const struct sbi_platform_operations platform_ops = {
-	.early_init = cheshire_early_init,
-	.final_init = cheshire_final_init,
+	.early_init   = cheshire_early_init,
+	.final_init   = cheshire_final_init,
 	.console_init = cheshire_console_init,
 	.irqchip_init = cheshire_irqchip_init,
-	.ipi_init = cheshire_ipi_init,
-	.timer_init = cheshire_timer_init,
+	.ipi_init     = cheshire_ipi_init,
+	.timer_init   = cheshire_timer_init,
 };
 
 const struct sbi_platform platform = {
-	.opensbi_version = OPENSBI_VERSION,
-	.platform_version = SBI_PLATFORM_VERSION(0x0, 0x01),
-	.name = "CHESHIRE RISC-V",
-	.features = SBI_PLATFORM_DEFAULT_FEATURES,
-	.hart_count = CHESHIRE_HART_COUNT,
-	.hart_stack_size = SBI_PLATFORM_DEFAULT_HART_STACK_SIZE,
+	.opensbi_version   = OPENSBI_VERSION,
+	.platform_version  = SBI_PLATFORM_VERSION(0x0, 0x01),
+	.name              = "CHESHIRE RISC-V",
+	.features          = SBI_PLATFORM_DEFAULT_FEATURES,
+	.hart_count        = CHESHIRE_HART_COUNT,
+	.hart_stack_size   = SBI_PLATFORM_DEFAULT_HART_STACK_SIZE,
+	.heap_size         = SBI_PLATFORM_DEFAULT_HEAP_SIZE(CHESHIRE_HART_COUNT),
 	.platform_ops_addr = (unsigned long)&platform_ops
 };
