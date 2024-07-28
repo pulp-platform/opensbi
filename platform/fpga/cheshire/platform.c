c0ad60f684ea6304eb8a807561d88f9051889b0d
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-07-28 17:56:53 +0200

    Reduce frequency to 40 MHz.

--- platform/fpga/cheshire/platform.c
+++ platform/fpga/cheshire/platform.c
@@ -19,7 +19,7 @@
 #include <sbi_utils/timer/aclint_mtimer.h>
 
 #define CHESHIRE_UART_ADDR	      0x03002000
-#define CHESHIRE_UART_FREQ	      50000000
+#define CHESHIRE_UART_FREQ	      40000000
 #define CHESHIRE_UART_BAUDRATE	      115200
 #define CHESHIRE_UART_REG_SHIFT	      2
 #define CHESHIRE_UART_REG_WIDTH	      4
