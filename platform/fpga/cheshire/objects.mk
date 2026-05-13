c897ee09a4b8e24b0c78f01536c0f3ac94e4fb0f
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2026-05-13 18:56:10 +0200

    platform: add Cheshire support

---
+++ platform/fpga/cheshire/objects.mk
@@ -0,0 +1,33 @@
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Copyright (C) 2019 FORTH-ICS/CARV
+#		Panagiotis Peristerakis <perister@ics.forth.gr>
+#
+
+# Compiler flags
+platform-cppflags-y =
+platform-cflags-y   =
+platform-asflags-y  =
+platform-ldflags-y  =
+
+# Object to build
+platform-objs-y += platform.o
+
+# Platform XLEN
+PLATFORM_RISCV_XLEN = 64
+
+# Blobs to build
+FW_TEXT_START=0x80000000
+
+# Dynamic firmware configuration
+FW_DYNAMIC=n
+
+# Firmware that jumps at fixed address
+FW_JUMP=n
+
+# Firmware with payload configuration.
+FW_PAYLOAD=y
+# FW_PAYLOAD_ALIGN=0x1000
+FW_PAYLOAD_OFFSET=0x200000        # This needs to be 2MB aligned for 64-bit support
+FW_PAYLOAD_FDT_ADDR=0x82200000    # TODO: this is a cancerous workaround; set this correctly, ideally built!
